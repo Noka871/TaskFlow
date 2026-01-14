52ea415ed996c21f0b254783724d1fe79f9bb8ff
Noka871 <58460996+Noka871@users.noreply.github.com>
2026-01-15 02:14:56 +0700

    Add files via upload

---
+++ taskflow-backend/src/Entity/Task.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TaskRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: TaskRepository::class)]
+class Task
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $title = null;
+
+    #[ORM\Column(type: 'text', nullable: true)]
+    private ?string $description = null;
+
+    #[ORM\Column]
+    private bool $isCompleted = false;
+
+    // Геттеры/сеттеры...
+}
